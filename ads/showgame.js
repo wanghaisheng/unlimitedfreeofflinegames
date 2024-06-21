08acd45f19bc2fac7736ed663b51754c9458bc9e
1000freegames <148357508+1000freegames@users.noreply.github.com>
2024-06-21 17:04:10 +0700

    Create showgame.js

---
+++ ads/showgame.js
@@ -0,0 +1,23 @@
+var cont="<a href='" +urls[randomgame]+ "'><img src='" + images[randomgame] + "'border=0 width=300 height=600></a>"+"<br><font size=4></font>"
+document.write(cont)
+/*
+     FILE ARCHIVED ON 17:49:15 May 20, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 10:12:19 Jan 07, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 72.991 (3)
+  esindex: 0.005
+  captures_list: 86.013
+  CDXLines.iter: 8.81 (3)
+  PetaboxLoader3.datanode: 71.28 (5)
+  exclusion.robots: 0.202
+  exclusion.robots.policy: 0.194
+  RedisCDXSource: 1.529
+  PetaboxLoader3.resolve: 374.928 (3)
+  load_resource: 432.356
+*/
