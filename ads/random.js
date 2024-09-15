81a000f1d1fa885fe2244fdf0969513b08e181c0
1000freegames <148357508+1000freegames@users.noreply.github.com>
2024-09-15 13:56:39 +0700

    Update random.js

--- ads/random.js
+++ ads/random.js
@@ -7,124 +7,124 @@ var urls=new Array(totallinks)
 var images=new Array(totallinks)
 var titles=new Array(totallinks)
 
-urls[0]="https://1000freegames.github.io/subway-surfers/beijing-2020.html"
+urls[0]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fbeijing-2020.html&sa=D&sntz=1&usg=AOvVaw3U0DxV_bKE6x9Sj75dtFgw"
 images[0]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/2/28/Beijing2020Splash.png"
 
-urls[1]="https://1000freegames.github.io/subway-surfers/zurich-2019.html"
+urls[1]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fzurich-2019.html&sa=D&sntz=1&usg=AOvVaw0q9Yjq0pIcr3y85vB-kmzw"
 images[1]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/1/15/Screenshot_20190404_203317_com.kiloo.subwaysurf.jpg"
 
-urls[2]="https://1000freegames.github.io/subway-surfers/san-francisco-2019.html"
+urls[2]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fsan-francisco-2019.html&sa=D&sntz=1&usg=AOvVaw14uXY3NKWHdl0w3CCxJ3sh"
 images[2]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/7/74/SanFrancisco2019Splash.png"
 
-urls[3]="https://1000freegames.github.io/subway-surfers/berlin-2018.html"
+urls[3]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fberlin-2018.html&sa=D&sntz=1&usg=AOvVaw0tXQMx8NAixHfyjfKv58C3"
 images[3]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/5/5b/BerlinSplash.jpg"
 
-urls[4]="https://1000freegames.github.io/subway-surfers/houston-2019.html"
+urls[4]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fhouston-2019.html&sa=D&sntz=1&usg=AOvVaw2DfWAm4WMz6nI-y8HmUnuH"
 images[4]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/5/52/HoustonSplash.png"
 
-urls[5]="https://1000freegames.github.io/subway-surfers/monaco-2018.html"
+urls[5]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fmonaco-2018.html&sa=D&sntz=1&usg=AOvVaw1tbLDmU9jPZm0SW4Ul6tqz"
 images[5]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/8/80/Monaco2018Splash.jpg"
 
-urls[6]="https://1000freegames.github.io/subway-surfers/seoul-2019.html"
+urls[6]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fseoul-2019.html&sa=D&sntz=1&usg=AOvVaw2a1TWVMVMuTZtQXOhnEzYM"
 images[6]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/3/31/Seoul2019Splash.png"
 
-urls[7]="https://1000freegames.github.io/subway-surfers/havana-2018.html"
+urls[7]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fhavana-2018.html&sa=D&sntz=1&usg=AOvVaw2Af8mHa1H9FsVzcxqP9_J3"
 images[7]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/8/8f/SubwaySurfersHavana2018Splash.png"
 
-urls[8]="https://1000freegames.github.io/subway-surfers/london-2018.html"
+urls[8]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Flondon-2018.html&sa=D&sntz=1&usg=AOvVaw0CpVON6vl7vibUD1Ju_4Au"
 images[8]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/9/96/London2018Splash.png"
 
-urls[9]="https://1000freegames.github.io/subway-surfers/new-orleans-2018.html"
+urls[9]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fnew-orleans-2018.html&sa=D&sntz=1&usg=AOvVaw2YUan2OB_euMA6gF1Gyll9"
 images[9]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/3/3f/NewOrleans2018Splash.jpeg"
 
-urls[10]="https://1000freegames.github.io/subway-surfers/saint-petersburg-2017.html"
+urls[10]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fsaint-petersburg-2017.html&sa=D&sntz=1&usg=AOvVaw1CvaWXr_a6qGFM7JIhWtxw"
 images[10]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/2/21/WorldTourStPetersburg.png"
 
-urls[11]="https://1000freegames.github.io/subway-surfers/atlanta-2019.html"
+urls[11]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fatlanta-2019.html&sa=D&sntz=1&usg=AOvVaw33so4y8M19GXzFAJmEjkA8"
 images[11]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/1/1b/AtlantaSplash.jpg"
 
-urls[12]="https://1000freegames.github.io/subway-surfers/singapore-2019.html"
+urls[12]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fsingapore-2019.html&sa=D&sntz=1&usg=AOvVaw12SbzVNnRy8C4rOzcTtCjf"
 images[12]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/6/6d/Singapore2019LoadingScreen.png"
 
-urls[13]="https://1000freegames.github.io/subway-surfers/mumbai-2018.html"
+urls[13]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fmumbai-2018.html&sa=D&sntz=1&usg=AOvVaw3QYvuM6ElMu8gDsSchF4eI"
 images[13]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/e/e9/SubwaySurfersMumbai2018Splash.png"
 
-urls[14]="https://1000freegames.github.io/subway-surfers/tokyo-2018.html"
+urls[14]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Ftokyo-2018.html&sa=D&sntz=1&usg=AOvVaw2D71sw_ykjatP49_osDvcc"
 images[14]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/6/60/SubwaySurfersWorldTourTokyo2018.png"
 
-urls[15]="https://1000freegames.github.io/subway-surfers/bali-2019.html"
+urls[15]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fbali-2019.html&sa=D&sntz=1&usg=AOvVaw3yEibYOBWlX_3-O-fofVS0"
 images[15]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/2/26/BaliSplash.png"
 
-urls[16]="https://1000freegames.github.io/subway-surfers/iceland-2020.html"
+urls[16]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Ficeland-2020.html&sa=D&sntz=1&usg=AOvVaw0uQTGUbsX4R6ENxaP7SYKk"
 images[16]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/8/82/Iceland2020_-_Loading_Screen.jpg"
 
-urls[17]="https://1000freegames.github.io/subway-surfers/marrakesh-2018.html"
+urls[17]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fmarrakesh-2018.html&sa=D&sntz=1&usg=AOvVaw2Fp4vUFHbOR17mF8dwTDf5"
 images[17]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/3/30/Marrakesh2018LoadingScreen.png"
 
-urls[18]="https://1000freegames.github.io/subway-surfers/hong-kong-2018.html"
+urls[18]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fhong-kong-2018.html&sa=D&sntz=1&usg=AOvVaw0bto2GVKWorkpZX278KQck"
 images[18]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/5/5d/Hong_Kong_Loading_Screen.jpg"
 
-urls[19]="https://1000freegames.github.io/subway-surfers/venice-2019.html"
+urls[19]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fvenice-2019.html&sa=D&sntz=1&usg=AOvVaw1uksgZSmC4THSLPO-TjUag"
 images[19]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/8/8a/Screenshot_2019-02-23-20-05-22.png"
 
-urls[20]="https://1000freegames.github.io/subway-surfers/buenos-aires-2020.html"
+urls[20]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fbuenos-aires-2020.html&sa=D&sntz=1&usg=AOvVaw3yEVilLTxlSLhTMIVymYGi"
 images[20]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/e/e2/SSWTBuenosAires.png"
 
-urls[21]="https://1000freegames.github.io/subway-surfers/new-york-2018.html"
+urls[21]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fnew-york-2018.html&sa=D&sntz=1&usg=AOvVaw1MUL8bGZwRKFlCVBilzOQo"
 images[21]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/b/b1/SubwaySurfersNewYork2018Splash.png"
 
-urls[22]="https://1000freegames.github.io/subway-surfers/winter-holiday-2019.html"
+urls[22]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fwinter-holiday-2019.html&sa=D&sntz=1&usg=AOvVaw09xcJQo3xfis-8oBjQV1SB"
 images[22]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/5/5d/Screenshot_2019-11-28-06-27-12.png"
 
-urls[23]="https://1000freegames.github.io/subway-surfers/chicago-2023.html"
+urls[23]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fchicago-2023.html&sa=D&sntz=1&usg=AOvVaw1SBdvU3NAhIlhu3_YJInlF"
 images[23]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/f/f0/ScreenChicago.webp"
 
-urls[24]="https://1000freegames.github.io/subway-surfers/chicago-2023.html"
+urls[24]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fchicago-2023.html&sa=D&sntz=1&usg=AOvVaw1SBdvU3NAhIlhu3_YJInlF"
 images[24]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/f/fa/SpraybotScreen.webp"
 
-urls[25]="https://1000freegames.github.io/subway-surfers/new-york-2023.html"
+urls[25]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fnew-york-2023.html&sa=D&sntz=1&usg=AOvVaw0vjcifVQFHrzHmzIT_UMgX"
 images[25]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/8/84/NewYork2023Screen.jpg"
 
-urls[26]="https://1000freegames.github.io/subway-surfers/new-york-2023.html"
+urls[26]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fnew-york-2023.html&sa=D&sntz=1&usg=AOvVaw0vjcifVQFHrzHmzIT_UMgX"
 images[26]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/5/57/RainScreen2023.jpg"
 
-urls[27]="https://1000freegames.github.io/subway-surfers/fantasy-fest-2023.html"
+urls[27]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Ffantasy-fest-2023.html&sa=D&sntz=1&usg=AOvVaw38s4lBDmVsd7BSIBGZ9w6L"
 images[27]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/b/b0/FantasyFestScreen.jpg"
 
-urls[28]="https://1000freegames.github.io/subway-surfers/rio-2023.html"
+urls[28]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Frio-2023.html&sa=D&sntz=1&usg=AOvVaw35ADPJXiL64Uz_miyxYUQy"
 images[28]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/0/06/RioScreen.jpg"
 
-urls[29]="https://1000freegames.github.io/subway-surfers/buenos-aires-2023.html"
+urls[29]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fbuenos-aires-2023.html&sa=D&sntz=1&usg=AOvVaw1wYPnMOiaYpAmfmil5rVGq"
 images[29]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/4/4b/BuenosAiresScreen.jpg"
 
-urls[30]="https://1000freegames.github.io/subway-surfers/tokyo-2023.html"
+urls[30]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Ftokyo-2023.html&sa=D&sntz=1&usg=AOvVaw0CNGkenddP4AyZvCiVLb0t"
 images[30]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/7/7e/TokyoScreen.jpg"
 
-urls[31]="https://1000freegames.github.io/subway-surfers/hawaii-2023.html"
+urls[31]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fhawaii-2023.html&sa=D&sntz=1&usg=AOvVaw2bP3An63_mhyWPVHf5x8Ek"
 images[31]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/8/8b/Hawaii2023%281%29.png"
 
-urls[32]="https://1000freegames.github.io/subway-surfers/haunted-hood-2023.html"
+urls[32]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fhaunted-hood-2023.html&sa=D&sntz=1&usg=AOvVaw323bKReXgMfMZr7xBqUtUP"
 images[32]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/6/62/Hauntedhoodgoogleplay1.png"
 
-urls[33]="https://1000freegames.github.io/subway-surfers/haunted-hood-2023.html"
+urls[33]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fhaunted-hood-2023.html&sa=D&sntz=1&usg=AOvVaw323bKReXgMfMZr7xBqUtUP"
 images[33]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/9/98/Hauntedhoodgoogleplay5.png"
 
-urls[34]="https://1000freegames.github.io/subway-surfers/seoul-2023.html"
+urls[34]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fseoul-2023.html&sa=D&sntz=1&usg=AOvVaw3bEILRpbMLN8BnQLp3zB-a"
 images[34]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/9/94/SeoulGooglePlay1.png"
 
-urls[35]="https://1000freegames.github.io/subway-surfers/seoul-2023.html"
+urls[35]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fseoul-2023.html&sa=D&sntz=1&usg=AOvVaw3bEILRpbMLN8BnQLp3zB-a"
 images[35]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/c/cc/SeoulGooglePlay5.png"
 
-urls[36]="https://1000freegames.github.io/subway-surfers/london-2023.html"
+urls[36]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Flondon-2023.html&sa=D&sntz=1&usg=AOvVaw1BvXBDEqNGAeEhHZSaOe0K"
 images[36]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/2/2a/PlayNowLondon2023.jpeg"
 
-urls[37]="https://1000freegames.github.io/subway-surfers/shenzhen-2024.html"
+urls[37]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Fshenzhen-2024.html&sa=D&sntz=1&usg=AOvVaw0vx4ZVsESp6-92zl2L8AX2"
 images[37]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/e/e9/PlayNowShenzhen2024.jpeg"
 
-urls[38]="https://1000freegames.github.io/subway-surfers/underwater-2024.html"
+urls[38]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Funderwater-2024.html&sa=D&sntz=1&usg=AOvVaw2jr2KN_E07mqWah3NVDYwP"
 images[38]="https://pocket-image-cache.com/300x600/https://static.wikia.nocookie.net/subwaysurf/images/2/24/Screen-0UNDERWATER.jpg"
 
-urls[39]="https://1000freegames.github.io/subway-surfers/underwater-2024.html"
+urls[39]="https://www.google.com/url?q=https%3A%2F%2F1000freegames.github.io%2Fsubway-surfers%2Funderwater-2024.html&sa=D&sntz=1&usg=AOvVaw2jr2KN_E07mqWah3NVDYwP"
 images[39]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/0b1e677c02a726763c044dfc2ad6e9fc.png"
 
 var randomgame=Math.floor(Math.random()*totallinks)
